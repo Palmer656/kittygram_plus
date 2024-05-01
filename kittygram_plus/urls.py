f70b5e1fe23af1c3429d7337c218df891b291c30
Дударев Михаил <Palmer656@yandex.ru>
2024-05-01 17:36:09 +0300

    финальная версия проекта

--- kittygram_plus/urls.py
+++ kittygram_plus/urls.py
@@ -13,5 +13,10 @@ router.register(r'mycats', LightCatViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-token-auth/', views.obtain_auth_token)
+    # path('api-token-auth/', views.obtain_auth_token)
+    # Djoser создаст набор необходимых эндпоинтов.
+    # базовые, для управления пользователями в Django:
+    path('auth/', include('djoser.urls')),
+    # JWT-эндпоинты, для управления JWT-токенами:
+    path('auth/', include('djoser.urls.jwt')),
 ]
